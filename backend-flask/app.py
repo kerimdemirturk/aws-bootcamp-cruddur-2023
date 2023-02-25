1b2adf04a5d237c08a0b2cf78f5c5de4dcd975f0
Kerim Demirturk <kerimdmrturk@gmail.com>
2023-02-25 14:27:52 +0000

    added docker files both for FRONTEND and BACKEND.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -16,6 +16,7 @@ from services.show_activity import *
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
+printf(frontend)
 origins = [frontend, backend]
 cors = CORS(
   app, 
